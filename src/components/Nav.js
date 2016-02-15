4c3af7bc1468f9036e7a1dd960c5006d35c44a52
lemming <alexey@tcherevatov.ru>
2016-02-15 18:11:25 +0300

    various fixes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,65 @@
 import React, { Component } from 'react';
 import { Router, Route, IndexRoute, Link, IndexLink } from 'react-router';
 
+function flatMap(array, fn) {
+    var result = [];
+
+    array.forEach((item, idx) => fn(item, idx).forEach(resultItem => result.push(resultItem)));
+
+    return result;
+}
+
+function replaceSpaces(parentKey, str, idx) {
+    const key = `${parentKey}.${idx}`;
+
+    if (/^\s+$/.test(str)) {
+        return str.split('').map((space, spaceIndex) => <span key={`${key}.${spaceIndex}`}> </span>);
+    }
+    return [<span className="text" key={key}>{str}</span>];
+}
+
+function linkRenderer(parentKey, text) {
+    return flatMap(text.split(/\n/), (chunk, idx) => {
+        return flatMap(chunk.split(/\b/), replaceSpaces.bind(this, `${parentKey}.${idx * 2}`))
+            .concat(<br key={`${parentKey}.${idx * 2 + 1}`} />);
+    });
+}
+
+const links = [{
+    to: '/releases',
+    text: (
+`Ab[Releas-
+es:
+  A1/16/_
+  Marfa/b1.1;
+  ]`
+    )
+}, {
+    to: '/codes',
+    text: (
+`Ab[Codes:
+   A; b;
+   S;c; r;
+   ee; n
+  ]`
+    )
+}, {
+    to: '/about',
+    text: `About;`
+}, {
+    to: '/editorial',
+    text: (
+`Ab[Edi-
+  torial]`
+    )
+}];
+
+const navItems = links.map((link, idx) => (
+    <li key={idx}>
+        <Link activeClassName="active" to={link.to}>{linkRenderer(idx, link.text)}</Link>
+    </li>
+));
+
 var Nav = (props) => {
     return (
         <nav className="leftbar">
@@ -11,35 +70,7 @@ var Nav = (props) => {
           </h1>
 
           <ul className="menu">
-            <li>
-              <Link to="/releases" activeClassName="active">
-                Ab[Releas-<br/>
-                es:<br/>
-                  A1/16/_<br/>
-                  Marfa/b1.1;<br/>
-                  ]<br/><br/>
-              </Link>
-            </li>
-            <li>
-              <Link to="/codes" activeClassName="active">
-                Ab[Codes:<br/>
-                   A; b;<br/>
-                   S;c; r;<br/>
-                   ee; n<br/>
-                  ]<br/><br/>
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" activeClassName="active">
-                About;<br/><br/>
-              </Link>
-            </li>
-            <li>
-              <Link to="/editorial" activeClassName="active">
-                Ab[Edi-<br/>
-                  torial]
-              </Link>
-            </li>
+            {navItems}
           </ul>
         </nav>
     );
--- src/components/RightPanel.js
+++ src/components/RightPanel.js
@@ -8,16 +8,18 @@ var RightPanel = () => {
 
             <ul className="links">
                 <li>
-                    <a href="mailto:ab@abscreenwear.com">email;</a>
+                    <a href="mailto:ab@abscreenwear.com"><span className="text">email;</span></a>
                 </li>
                 <li>
-                    <a href="https://www.instagram.com/abscreenwear/" target="_blank">ig;</a>
+                    <a href="https://www.instagram.com/abscreenwear/" target="_blank">
+                        <span className="text">ig;</span>
+                    </a>
                 </li>
                 <li>
-                    <a href="https://www.facebook.com/AbScreenwear-1656694317934158/" target="_blank">fb;   </a>
+                    <a href="https://www.facebook.com/AbScreenwear-1656694317934158/" target="_blank"><span className="text">fb;</span>   </a>
                 </li>
                 <li>
-                    <a href="https://abscreenwear.tumblr.com" target="_blank">Tumblr</a>
+                    <a href="https://abscreenwear.tumblr.com" target="_blank"><span className="text">Tumblr</span></a>
                 </li>
                 <li>]   </li>
             </ul>
