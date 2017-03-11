2e2f1efa9e54114bf32bf98267594c0b28099648
Jeff Jackson <jeffmjack@gmail.com>
2017-03-11 16:06:46 -0500

    remove subscribe form from homepage

--- src/pages/Screenwear.js
+++ src/pages/Screenwear.js
@@ -5,7 +5,6 @@ class Screewear extends React.Component {
     render() {
         return (
             <div style={{ position: 'relative' }}>
-                <SubscribeForm/>
                 <img style={{ width: '100%' }} src={require('../images/spencer.gif')}/>
             </div>
         );
