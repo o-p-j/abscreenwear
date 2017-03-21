46d6f9694baece82aff6f4433f20e7570a760c3d
Jeff Jackson <jeffmjack@gmail.com>
2017-03-21 14:49:46 -0400

    update release gallery for 31 kahn jpgs

--- src/pages/Releases.js
+++ src/pages/Releases.js
@@ -28,19 +28,20 @@ class Releases extends React.Component {
 
     render() {
         const items = [];
-            for (let num = 0; num < 10; num++) {
 
-              if (this.props.params.release === 'marfa'){
+          if (this.props.params.release === 'marfa'){
+              for (let num = 0; num < 10; num++) {
                 items.push(require(`../images/marfa/Ab_marfa${num}.jpg`));
               }
+          }
 
-              if (this.props.params.release === 'kahn'){
+          if (this.props.params.release === 'kahn' || 'postmasters'){
+              for (let num = 0; num < 31; num++) {
                 items.push(require(`../images/kahn/AbScreenwear_Kahn${num}.jpg`));
               }
+          }
 
-              else console.log('I don\'t have images for the gallery you\'re looking for')
-
-            }
+          else console.log('I don\'t have images for the gallery you\'re looking for')
 
         const images = items.concat(items).map((src, idx) => <img key={idx} src={src}/>);
         if (this.props.params.release) {
