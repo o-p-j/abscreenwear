bc40066e79325fe1161d1aa127ae7125a615f17f
Andrew McCarthy <hi@andrevv.com>
2017-03-23 18:43:15 +0100

    static

---
+++ src/components/PostmastersCredits.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+var PostmastersCredits = () => {
+
+    const cursor = {
+        cursor: 'url('+require('../images/postmasters/cursor.png')+') 15 15, auto'
+    };
+
+    return (
+        <div>
+        <p>
+            photography<br/>
+            <a style={cursor} href="https://www.instagram.com/benmorrisphoto" target="_blank">@benmorrisphoto</a>
+        </p>
+        <p>
+            video<br/>
+            <a style={cursor} href="https://www.instagram.com/bedj" target="_blank">@bedj</a>
+            <br/>
+            <a style={cursor} href="https://www.instagram.com/vide0queens" target="_blank">@vide0queens</a>
+        </p>
+        <p>
+            makeup/hair<br/>
+            <a style={cursor} href="https://www.instagram.com/harkoff_makeup" target="_blank">@harkoff_makeup</a>
+        </p>
+        <p>
+            styling<br/>
+            <a style={cursor} href="https://www.instagram.com/rogerforlovers" target="_blank">@rogerforlovers</a>
+            
+        </p>
+        <p>
+            creative direction<br/>
+            <a style={cursor} href="https://www.instagram.com/jamesorlandooo" target="_blank">@jamesorlandooo</a>
+
+        </p>
+        <p>
+            web<br/>
+            <a style={cursor} href="https://www.instagram.com/andrevvm" target="_blank">@andrevvm</a>
+        </p>
+        <p>
+            featuring<br/>
+            <a style={cursor} href="https://www.instagram.com/elliottsailors" target="_blank">@elliottsailors</a>
+            <br/>
+            <a style={cursor} href="https://www.instagram.com/ashbaccus" target="_blank">@ashbaccus</a>
+            <br/>
+            <a style={cursor} href="https://www.instagram.com/danielledorchester" target="_blank">@danielledorchester</a>
+        </p>
+        <p>
+            photo assistants<br/>
+            <a style={cursor} href="https://www.instagram.com/rossthomas" target="_blank">@rossthomas</a>
+            <br/>
+            <a style={cursor} href="https://www.instagram.com/jslasswell" target="_blank">@jslasswell</a>
+        </p>
+        <p>
+            stylist assistants<br/>
+            <a style={cursor} href="https://www.instagram.com/_kevinhuynh_" target="_blank">@_kevinhuynh_</a>
+            <br/>
+            <a style={cursor} href="https://www.instagram.com/joenycgonzalez" target="_blank">@joenycgonzalez</a>
+        </p>
+        <p>
+            <a style={cursor} href="https://www.instagram.com/abscreenwear" target="_blank">@abscreenwear</a>
+        </p>
+        </div>
+    );
+};
+
+export default PostmastersCredits;
