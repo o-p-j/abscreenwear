e2200e80ac42b403af34b3ba9c2a53f40d8e7865
Andrew McCarthy <hi@andrevv.com>
2017-08-17 11:52:18 +0200

    gif size

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ import cursorPlay from '../images/postmasters/cursor_play.png';
 import { isMobile } from '../utils/is-mobile'
 
 const Home = (props) => {
-    const videoUrl = `https://player.vimeo.com/video/209747767?title=0&byline=0&portrait=0&playsinline=1&autoplay=1${isMobile ? '' : '&background=1'}`
     return (
         <img className="c-home-img" src={require('../images/N02_WHITEv1.gif')} />
     )
