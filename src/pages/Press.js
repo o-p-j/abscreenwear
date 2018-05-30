f8d50ba6d37eda97f3e6c23fa6e8bac85e1af32c
Andrew McCarthy <hi@andrevv.com>
2018-05-30 20:34:36 +0200

    press cont.

--- src/pages/Press.js
+++ src/pages/Press.js
@@ -21,7 +21,7 @@ class Press extends ParallaxPage {
                 <p>
                   <a href="http://su-la-po.com/content/2018/the-future-females-and-fashion" target="_blank">su-la-po.com/content/2018/the-future-females-and-fashion</a>
                 </p>
-                <img className="parallax" width="70.5%" style={{ mixBlendMode: 'multiply' }} src={ require('../images/pressV2/01/sulapo.png') } />
+                <img className="parallax transparent" width="70.5%" style={{ }} src={ require('../images/pressV2/01/sulapo.png') } />
                 <img width="83%" src={ require('../images/pressV2/01/1.jpg') } />
                 <img className="parallax layer-2" width="24%" style={{ marginLeft: '0%', marginTop: '14%' }} src={ require('../images/pressV2/01/thefuture.png') } />
                 <img className="parallax layer-1" width="18%" style={{ marginLeft: '35.5%', marginTop: '27%' }} src={ require('../images/pressV2/01/females.png') } />
@@ -69,7 +69,7 @@ class Press extends ParallaxPage {
                   <a href="http://addresspublications.com/get-a-room-exhibition" target="_blank">addresspublications.com/get-a-room-exhibition</a>
                 </p>
                 <img className="parallax layer-2" width="94%" style={{ marginLeft: '-4%', marginTop: '1%' }} src={ require('../images/pressV2/05/address.png') } />
-                <img className="" width="83%" style={{ marginTop: '10%', zIndex: 5, mixBlendMode: 'multiply' }} src={ require('../images/pressV2/05/01.jpg') } />
+                <img className="transparent" width="83%" style={{ marginTop: '10%', zIndex: 5 }} src={ require('../images/pressV2/05/01.jpg') } />
                 <img className="parallax" width="83%" style={{ marginTop: '-14%', zIndex: 0 }} src={ require('../images/pressV2/05/text.png') } />
               </div>
               <div className="press-item">
@@ -79,7 +79,7 @@ class Press extends ParallaxPage {
                   <a href="http://officemagazine.net/abscreenwear-and-your-bodys-full-potential" target="_blank">officemagazine.net/abscreenwear-and-your-bodys-full-potential</a>
                 </p>
                 <img className="" width="47%" style={{ marginTop: '1%' }} src={ require('../images/pressV2/06/office.png') } />
-                <img className="parallax layer-2" width="58%" style={{ marginTop: '28%', mixBlendMode: 'multiply' }} src={ require('../images/pressV2/06/text.png') } />
+                <img className="parallax transparent layer-2" width="58%" style={{ marginTop: '28%' }} src={ require('../images/pressV2/06/text.png') } />
                 <img width="83%" style={{ marginTop: '', zIndex: 0 }} src={ require('../images/pressV2/06/01.jpg') } />
               
               </div>
@@ -89,11 +89,95 @@ class Press extends ParallaxPage {
                 <p>
                   <a href="http://elle.ru/moda/fashion-blog/tehnika-molodeji-vyisokie-tehnologii-v-mode/" target="_blank">elle.ru/moda/fashion-blog/tehnika-molodeji-vyisokie-tehnologii-v-mode/</a>
                 </p>
-                <img className="" width="47%" style={{ marginTop: '1%' }} src={ require('../images/pressV2/06/office.png') } />
-                <img className="parallax layer-2" width="58%" style={{ marginTop: '28%', mixBlendMode: 'multiply' }} src={ require('../images/pressV2/06/text.png') } />
-                <img width="83%" style={{ marginTop: '', zIndex: 0 }} src={ require('../images/pressV2/06/01.jpg') } />
+                <img className="layer-2 transparent" width="34%" style={{ marginTop: '1%', zIndex: 25 }} src={ require('../images/pressV2/07/01.png') } />
+
+                <img className="parallax layer-2" width="74%" style={{ marginLeft: '-3%', marginTop: '35%' }} src={ require('../images/pressV2/07/02.png') } />
+
+                <img className="parallax layer-2" width="27%" style={{ marginLeft: '30%', marginTop: '16%' }} src={ require('../images/pressV2/07/03.png') } />
+
+                <img width="73%" style={{ marginTop: '', zIndex: 0 }} src={ require('../images/pressV2/07/04.png') } />
+
+                <img width="83%" style={{ marginTop: '', zIndex: 0 }} src={ require('../images/pressV2/07/05.jpg') } />
               
               </div>
+
+              <div className="press-item">
+
+                <p>Wall Street International</p>
+                <p>
+                  <a href="https://wsimag.com/fashion/31918-ab-screenwear" target="_blank">wsimag.com/fashion/31918-ab-screenwear</a>
+                </p>
+                <img className="layer-2 transparent" width="20%" style={{ marginTop: '1%', zIndex: 25 }} src={ require('../images/pressV2/08/01.png') } />
+                <br/>
+                <img className="" width="78%" style={{ marginLeft: '6%', marginTop: '-5%' }} src={ require('../images/pressV2/08/03.jpg') } />
+
+                <img className="parallax transparent layer-2" width="32%" style={{ marginTop: '22%' }} src={ require('../images/pressV2/08/02.png') } />
+              
+              </div>
+
+              <div className="press-item">
+
+                <p>Bullet magazine</p>
+                <p>
+                  <a href="http://bullettmedia.com/article/studio-visit-abscreenwear-reinvents-wearable-tech/" target="_blank">bullettmedia.com/article/studio-visit-abscreenwear-reinvents-wearable-tech/</a>
+                </p>
+                <img className="layer-1" width="95%" style={{ marginTop: '28%' }} src={ require('../images/pressV2/09/02.jpg') } />
+                <img className="parallax transparent layer-2" width="86%" style={{ marginLeft: '-9%', marginTop: '1%', zIndex: 25 }} src={ require('../images/pressV2/09/01.png') } />
+                
+              
+              </div>
+
+              <div className="press-item">
+
+                <p>Harper's Bazaar</p>
+                <p>
+                  print
+                </p>
+                <img className="layer-1" width="56%" style={{ }} src={ require('../images/pressV2/10/01.png') } />
+                <img className="parallax layer-2" width="34%" style={{ marginLeft: '54%', marginTop: '35%', zIndex: 25 }} src={ require('../images/pressV2/10/03.png') } />
+                <img className="layer-1" width="100%" style={{marginLeft: '-4%' }} src={ require('../images/pressV2/10/02.jpg') } />
+                
+              </div>
+
+              <div className="press-item">
+
+                <img className="parallax layer-1" width="95%" style={{ zIndex: 0 }} src={ require('../images/pressV2/11/03.jpg') } />
+                <p style={{ position: 'relative', zIndex: 0 }}>The Cut</p>
+                <p style={{ position: 'relative', zIndex: 0 }}>
+                  <a href="https://www.thecut.com/2017/02/ab-screenwear-olya-petrova-jackson-interview-nyfw.html" target="_blank">nymag.com/thecut/2017/02/ab-screen-wear-olya-petrova-jackson-interview-nyfw.html</a>
+                </p>
+                
+                <img className="transparent" width="57%" style={{ zIndex: 0 }} src={ require('../images/pressV2/11/01.png') } />
+
+                <br/>
+
+                <img className="transparent" width="22%" style={{ }} src={ require('../images/pressV2/11/05.png') } />
+
+                <img className="layer-1 transparent" width="77%" style={{ marginLeft: '5%', marginTop: '20%' }} src={ require('../images/pressV2/11/04.png') } />
+                
+              </div>
+
+              <div className="press-item">
+
+                
+                <p>The Cut</p>
+                <p>
+                  <a href="https://www.wgsn.com/blogs/ny-tech-forward-fashion-label-ab-screenwear/" target="_blank">wgsn.com/blogs/ny-tech-forward-fashion-label-ab-screenwear</a>
+                </p>
+                
+                <img className="" width="48%" style={{ zIndex: 0 }} src={ require('../images/pressV2/12/01.png') } />
+
+                <img className="parallax layer-2" width="60%" style={{ marginTop: '40%', marginLeft: '4%' }} src={ require('../images/pressV2/12/03.png') } />
+
+                <img className="parallax layer-1" width="50%" style={{ marginTop: '30%', marginLeft: '32%' }} src={ require('../images/pressV2/12/04.png') } />
+
+                <img className="" width="78%" style={{ position: 'relative', zIndex: 0 }} src={ require('../images/pressV2/12/02.png') } />
+
+
+                <img className="" width="75%" style={{ }} src={ require('../images/pressV2/12/05.png') } />
+                
+              </div>
+
             </div>
 
 
